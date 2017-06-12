38635d61ba4e10667a531a2304512dab37026b9f
fabio ROCCO <fabio.rocco@green-conseil.com>
2017-06-12 15:09:01 +0200

    Add animate + hover play stop

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ var pauseDefault = false;
 var slideshow = document.getElementById('slideshow');
 var slideshowChild = slideshow.children;
 var listElement = [];
+var reverse = 0;
 // create a simple instance
 var mc = new Hammer(slideshow);
 
@@ -23,27 +24,38 @@ mc.on("panleft panright tap", function(ev) {
     if (ev.type === "tap") {}
 });
 
-$("#next").click(nextImage);
-var reverse = 1;
-function nextImage() {
-    if (reverse == 1) {
-        changePreviousImg();
-        $('#rail').animate({"margin-right":"-800px"}, timeInterval);
-    }
-}
+setInterval(nextImage, timeInterval);
 
-function changePreviousImg() {
-    $('#rail').css('margin-right', '0px');
-    $('#rail img:first').before($('#rail img:last'));
+//$("#next").click(nextImage);
+
+$("#previous").click(() => {
+    reverse = 1;
+    nextImage();
+});
+$("#next").click(() => {
+    reverse = 0;
+    nextImage();
+});
+
+function nextImage() {
+    if (startDefault == true) {
+        if (reverse == 0) {
+            $('#rail').animate({"margin-left":"-800px"}, timeInterval,changeFirstImg);
+        } else {
+            changePreviousImg();
+            $('#rail').animate({"margin-right":"-800px"}, timeInterval);
+        }
+    }
 }
 
 function changeFirstImg() {
     $('#rail').css('margin-left', '0px');
     $('#rail img:last').after($('#rail img:first'));
-
 }
 
-setInterval(nextImage, 2000);
+function changePrevioustImg() {
+    $('#rail img:first').before($('#rail img:last'));
+}
 
 function getImages() {
     var url = "https://www.skrzypczyk.fr/slideshow.php";
@@ -55,3 +67,18 @@ function getImages() {
         })
     })
 }
+
+// Mettre sur pause/start au hover
+$('#rail').mouseenter(
+    () => {startDefault = false;}
+)
+$('#rail').mouseleave(
+    () => {startDefault = true;}
+)
+// Mettre sur pause/start au click
+$("#pause").click(() => {
+    startDefault = false;
+});
+$("#play").click(() => {
+    startDefault = true;
+});
