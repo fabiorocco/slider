4461ba170f3280ee8e8e291a12dbccf1cb50dc99
fabio ROCCO <fabio.rocco@green-conseil.com>
2017-06-16 20:25:43 +0200

    Custom beauty on the slider + animation

--- js/script.js
+++ js/script.js
@@ -1,84 +1,100 @@
 /**
  * Definition of the variables
  */
-
-var timeInterval = 2000;
+var timeInterval = 4000;
 var timeTransition = 2000;
-var startDefault = true;
-var pauseDefault = false;
+var autoMode = true;
 var slideshow = document.getElementById('slideshow');
 var slideshowChild = slideshow.children;
 var listElement = [];
-var reverse = 0;
 // create a simple instance
 var mc = new Hammer(slideshow);
-
 // listen to events...
 mc.on("panleft panright tap", function(ev) {
+    console.log(ev.type);
     if (ev.type === "panright") {
-
+        alert(1);
+        nextImage();
     }
-
-    if (ev.type === "panleft") {}
-
+    if (ev.type === "panleft") {
+        previousImage();
+    }
     if (ev.type === "tap") {}
 });
-
-setInterval(nextImage, timeInterval);
-
-//$("#next").click(nextImage);
-
-$("#previous").click(() => {
-    reverse = 1;
-    nextImage();
-});
-$("#next").click(() => {
-    reverse = 0;
-    nextImage();
-});
-
-function nextImage() {
-    if (startDefault == true) {
-        if (reverse == 0) {
-            $('#rail').animate({"margin-left":"-800px"}, timeInterval,changeFirstImg);
-        } else {
-            changePreviousImg();
-            $('#rail').animate({"margin-right":"-800px"}, timeInterval);
-        }
+function init(){
+    if(autoMode){
+        $("#play").hide();
+    }else{
+        $("#pause").hide();
     }
 }
+init();
 
-function changeFirstImg() {
-    $('#rail').css('margin-left', '0px');
-    $('#rail img:last').after($('#rail img:first'));
+$("#next").click(() => {
+    nextImage();
+});
+$("#previous").click(() => {
+    previousImage();
+});
+function fAutoMode(){
+    if(autoMode){
+        nextImage();
+    }
 }
-
-function changePrevioustImg() {
-    $('#rail img:first').before($('#rail img:last'));
+function nextImage() {
+    $('#rail').animate({"margin-left":"-800px"}, timeTransition, () => {
+        $('#rail').css('margin-left', '0px');
+        $('#rail .img:last').after($('#rail .img:first'));
+    });
 }
-
+function previousImage(){
+    $('#rail').css('margin-left', '-800px');
+    $('#rail .img:first').before($('#rail .img:last'));
+    $('#rail').animate({"margin-left":"0px"}, timeTransition, () => {
+        clearInterval(functionInterval);
+    });
+}
 function getImages() {
+    var width = 800;
     var url = "https://www.skrzypczyk.fr/slideshow.php";
     $.getJSON(url, {
         format: "json"
     }).done((data) => {
         $.each(data, (key, item) => {
-            $("<img>").attr({"id": key,"src": item.url, "alt": item.desc, "data-title": item.title}).appendTo("#rail");
+            width += 800;
+            $("#slideshow").css("min-width", "800px");
+            $("#rail").css("width", width + "px");
+            $("<div>").attr({"id": key, "alt": item.desc, "data-title": item.title, "class": "img"}).appendTo("#rail");
+            $("#"+key).css("background-image", "url("+ item.url +")");
         })
     })
 }
-
+var functionInterval = setInterval( fAutoMode, timeInterval);
 // Mettre sur pause/start au hover
-$('#rail').mouseenter(
-    () => {startDefault = false;}
+$('#slideshow').mouseenter(
+    () => {
+        //alert($('#pause').is(':visible'));
+        autoMode = false;
+        $(".contain-actions").css('visibility', 'visible');
+    }
 )
-$('#rail').mouseleave(
-    () => {startDefault = true;}
+$('#slideshow').mouseleave(
+    () => {
+        autoMode = $('#pause').is(':visible') == true ? true : false;
+        //alert($('#pause').is(':visible'));
+        //autoMode = $('#pause').is(:visible) == true ? false : true;
+        $(".contain-actions").css('visibility', 'hidden');
+    }
 )
-// Mettre sur pause/start au click
-$("#pause").click(() => {
-    startDefault = false;
+$(".contain-actions").css('visibility', 'hidden');
+
+$("#pause").click(function(){
+    autoMode = false;
+    $("#pause").hide();
+    $("#play").show();
 });
-$("#play").click(() => {
-    startDefault = true;
+$("#play").click(function(){
+    autoMode = true;
+    $("#pause").show();
+    $("#play").hide();
 });
