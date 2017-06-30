143a9b58de6050f1114d944591f85a207387afd5
Jérémy Bachayani <jeremy.bachayani@gmail.com>
2017-06-30 17:52:32 +0200

    add indicator

--- js/script.js
+++ js/script.js
@@ -2,10 +2,9 @@
  * Definition of the variables
  */
 
-var timeInterval = 2000;
+var timeInterval = 4000;
 var timeTransition = 2000;
 var startDefault = true;
-var pauseDefault = false;
 var slideshow = document.getElementById('slideshow');
 var slideshowChild = slideshow.children;
 var listElement = [];
@@ -24,37 +23,48 @@ mc.on("panleft panright tap", function(ev) {
     if (ev.type === "tap") {}
 });
 
-setInterval(nextImage, timeInterval);
-
-//$("#next").click(nextImage);
-
+$("#next").click(nextImage);
 $("#previous").click(() => {
     reverse = 1;
-    nextImage();
-});
-$("#next").click(() => {
-    reverse = 0;
-    nextImage();
-});
+})
 
 function nextImage() {
     if (startDefault == true) {
         if (reverse == 0) {
-            $('#rail').animate({"margin-left":"-800px"}, timeInterval,changeFirstImg);
+            $('#rail').animate({"margin-left":"-800px"}, timeTransition, changeFirstImg);
         } else {
-            changePreviousImg();
-            $('#rail').animate({"margin-right":"-800px"}, timeInterval);
+            alert(reverse);
+            $('#rail').animate({"margin-right":"0px"}, timeTransition, changePrevioustImg);
         }
     }
 }
 
 function changeFirstImg() {
+  var idOfImages = $('#rail .img:first').attr('data-type');
+  var idOfPuce = $('.indicator-item').attr('data-type');
+
     $('#rail').css('margin-left', '0px');
-    $('#rail img:last').after($('#rail img:first'));
+    $('#rail .img:last').after($('#rail .img:first'));
+
+    var arrayOfPuces = [];
+    $('.indicator-item').each(function() {
+      arrayOfPuces.push($(this).attr('data-type'));
+    });
+    var arrayOfIdImmage = [];
+    $('#rail .img:first').each(function() {
+      arrayOfPuces.push($(this).attr('data-type'));
+    });
+    
+    for (var i = 0; i < arrayOfPuces.length; i++) {
+      if (idOfImages == arrayOfPuces[i]) {
+        $('.indicator-item[data-type="'+arrayOfPuces[i]+'"]').toggleClass("indicator-active");
+      }
+    }
 }
 
 function changePrevioustImg() {
-    $('#rail img:first').before($('#rail img:last'));
+    $('#rail').css('margin-right', '0px');
+    $('#rail .img:first').before($('#rail .img:last'));
 }
 
 function getImages() {
@@ -63,11 +73,17 @@ function getImages() {
         format: "json"
     }).done((data) => {
         $.each(data, (key, item) => {
-            $("<img>").attr({"id": key,"src": item.url, "alt": item.desc, "data-title": item.title}).appendTo("#rail");
+            $("<div>").attr({"id": key, "data-description": item.desc, "data-title": item.title,"data-type": key, "class": "img"}).appendTo("#rail");
+            $("#"+key).css("background-image", "url("+ item.url +")");
+            $("<li>").attr({"class": "indicator-item", "data-type": key}).appendTo(".indicator");
+            $("<div></div>").appendTo("#"+key);
         })
     })
 }
 
+setInterval(nextImage, timeInterval);
+
+/*
 // Mettre sur pause/start au hover
 $('#rail').mouseenter(
     () => {startDefault = false;}
@@ -82,3 +98,4 @@ $("#pause").click(() => {
 $("#play").click(() => {
     startDefault = true;
 });
+*/
